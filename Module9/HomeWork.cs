21b6ce3081e204dcfe56e32a484224b07e79be9a
Wisperson <wisperson7@gmail.com>
2023-12-04 11:40:54 +0600

    Добавил домашку

---
+++ Module9/HomeWork.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module9
+{
+    public class HomeWork
+    {
+        static public void StartHW()
+        {
+            Random random = new Random();
+            List<Employee> employees = new List<Employee>();
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    employees.Add(new Manager("Ivan", random.Next(100000, 200000), Math.Round(random.NextDouble(), 2)));
+                }
+                else
+                {
+                    employees.Add(new Developer("Sergey", random.Next(300000, 1000000), random.Next(100, 1000)));
+                }
+            }
+            foreach (Employee emp in employees)
+            {
+                emp.CalculateAnnualSalary();
+            }
+
+        }
+
+        public class Employee
+        {
+            public string Name { get; set; }
+            public int Salary { get; set; }
+            public virtual void CalculateAnnualSalary()
+            {
+                Console.WriteLine(Salary);
+            }
+            public Employee(string name, int salary)
+            {
+                Name = name;
+                Salary = salary;
+            }
+        }
+
+        public class Manager: Employee
+        {
+            public double Bonus { get; set; }
+            public override void CalculateAnnualSalary()
+            {
+                Console.WriteLine(GetType().Name + ":" + Salary * (1 + Bonus));
+            }
+            public Manager (string name, int salary, double bonus) : base(name, salary)
+            {
+                Bonus = bonus;
+            }
+        }
+
+        public class Developer: Employee
+        {
+            public int LinesOfCodePerDay { get; set; }
+            public override void CalculateAnnualSalary()
+            {
+                Console.WriteLine(GetType().Name + ":" + (Salary + Salary * 0.000001 * LinesOfCodePerDay));
+            }
+            public Developer (string name, int salary, int lines) : base(name, salary)
+            {
+                LinesOfCodePerDay = lines;
+            }
+        }
+    }
+}
--- Module9/Program.cs
+++ Module9/Program.cs
@@ -13,65 +13,9 @@ namespace Module9
     {
         static void Main(string[] args)
         {
-            ArrayList list = new ArrayList();
-            list.Add(5);
-            list.Add(true);
-            list.Add("test");
-
-            Queue<int> tay = new Queue<int>();
-            for (int i = 0; i < 10;  i++)
-            {
-                tay.Enqueue(i);
-            }
-            Console.WriteLine("Queue: ");
-            foreach (var item in tay)
-            {
-                Console.WriteLine(item);
-            }
-
-            //---------------
-
-            Stack<int> st = new Stack<int>();
-            for (int i = 0; i < 10; i++)
-            {
-                st.Push(i);
-            }
-            foreach(var item in st)
-            {
-                Console.WriteLine(item);
-            }
-
-            //--------------
-
-            Hashtable ht = new Hashtable();
-            for (int i = 0; i < 10; i++)
-            {
-                if (!ht.ContainsKey(i)) { ht.Add(i, i); }
-            }
-
-            //----------------
-
-            Dictionary<int, int> dck = new Dictionary<int, int>();
-
-            //----------------
-
-            List<student> students = new List<student>();
-            students.Add(new student(0, "Ivan"));
-            students.Add(new student(1, "Bober"));
-
+            HomeWork.StartHW();
+            Console.ReadKey();
         }
     }
 
-    public class student
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-
-        public student(int id, string name)
-        {
-            this.id = id;
-            this.name = name;
-        }
-
-    }
 }
